ce02947c739b6d7ce1b3c654546a75bcb0d1bd9f
Alper Seyman <alperseyman@gmail.com>
2025-05-09 21:15:42 +0200

    4

---
+++ Polymorphism/Main.java
@@ -0,0 +1,25 @@
+
+
+// Polymorphism = "POLY" = "MANY"
+//                "MORPH" = "SHAPE"
+//                Objects can identify as other objects.
+//                Objects can be treated as objects of a common superclass.
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		Bike bike = new Bike();
+		Boat boat = new Boat();
+		
+		
+		Vehicle[] vehicles = {car, bike, boat};
+		
+		for(Vehicle vehicle: vehicles) {
+			vehicle.go();
+		}
+		
+	}
+}
